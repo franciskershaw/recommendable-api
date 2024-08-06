4ca75968c0ff1a931d3c4091f41ac7918ffa1a72
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-08-06 21:42:04 +0100

    Add local strategy

--- config/passport.js
+++ config/passport.js
@@ -1,14 +1,17 @@
 const passport = require("passport");
 const { ExtractJwt, Strategy: JwtStrategy } = require("passport-jwt");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 
-const options = {
+// JWT Strategy
+const jwtOptions = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: process.env.JWT_SECRET,
 };
 
 passport.use(
-  new JwtStrategy(options, async (jwt_payload, done) => {
+  new JwtStrategy(jwtOptions, async (jwt_payload, done) => {
     try {
       const user = await User.findById(jwt_payload.id);
       if (user) {
@@ -21,6 +24,31 @@ passport.use(
       return done(error, false);
     }
   })
+);
+
+// Local Strategy
+passport.use(
+  new LocalStrategy(
+    { usernameField: "email" },
+    async (email, password, done) => {
+      try {
+        const user = await User.findOne({ email });
+        if (!user) {
+          return done(null, false, { message: "Incorrect email or password" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+          return done(null, false, { message: "Incorrect email or password" });
+        }
+
+        return done(null, user);
+      } catch (error) {
+        console.error(error);
+        return done(error, false);
+      }
+    }
+  )
 );
 
 module.exports = passport;
