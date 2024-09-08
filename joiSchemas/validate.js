4a2152d4a274fd398d58804bd3ab2b1430d38a83
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-09-08 20:13:17 +0100

    Rename validate folder and fix typo

---
+++ joiSchemas/validate.js
@@ -0,0 +1,11 @@
+const { BadRequestError } = require("../utils/errors");
+
+const validateRequest = (payload, schema) => {
+  const { value, error } = schema.validate(payload);
+  if (error) {
+    throw new BadRequestError(error.details[0].message);
+  }
+  return value;
+};
+
+module.exports = validateRequest;
