6778d90837a0b10e65ad0f9cff4366cb5ac42875
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-09-16 22:23:56 +0100

    Recommend schema

--- joiSchemas/schemas.js
+++ joiSchemas/schemas.js
@@ -1,8 +1,31 @@
 const Joi = require("joi");
+const {
+  CATEGORY_FILMS,
+  CATEGORY_TV,
+  CATEGORY_MUSIC,
+  CATEGORY_EVENTS,
+  CATEGORY_PLACES,
+} = require("../utils/constants");
 
-const categorySchema = Joi.object({
-  name: Joi.string().required().min(1).max(100),
-  icon: Joi.string().required().min(1).max(12),
+const recommendSchema = Joi.object({
+  name: Joi.string().required().messages({
+    "any.required": "Name is required.",
+  }),
+  recommendedBy: Joi.string().required().messages({
+    "any.required": "RecommendedBy is required.",
+  }),
+  category: Joi.string()
+    .valid([
+      CATEGORY_FILMS,
+      CATEGORY_TV,
+      CATEGORY_MUSIC,
+      CATEGORY_EVENTS,
+      CATEGORY_PLACES,
+    ])
+    .required()
+    .messages({
+      "any.required": "Category is required.",
+    }),
 });
 
-module.exports = { categorySchema };
+module.exports = { recommendSchema };
