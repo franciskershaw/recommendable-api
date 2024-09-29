8a33b310b27460da723a9953bb42b2e1877a40fe
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-09-29 11:09:57 +0100

    Typo in recommend route

--- src/routes/recommends.ts
+++ src/routes/recommends.ts
@@ -1,5 +1,9 @@
 import express from "express";
-import { getRecommends, createRecommend } from "../controllers/recommends";
+import {
+  getRecommends,
+  createRecommend,
+  editRecommend,
+} from "../controllers/recommends";
 import { authenticateToken } from "../middleware/authMiddleware";
 
 const router = express.Router();
@@ -10,6 +14,6 @@ router.get("/", authenticateToken, getRecommends);
 // Create a new recommendation
 router.post("/", authenticateToken, createRecommend);
 
-router.put("/:id", authenticateToken, createRecommend);
+router.put("/:id", authenticateToken, editRecommend);
 
 export default router;
